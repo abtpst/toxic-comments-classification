8cb29b9c236b5b6d0341454e608114252eaf02d2
Abhijit-Tomar <Abhijit.Tomar@ibm.com>
2018-04-05 17:00:34 -0400

    began working on derived features

--- src/feature_engineering/FeaturesFromContent.py
+++ src/feature_engineering/FeaturesFromContent.py
@@ -10,32 +10,39 @@ import progressbar
 progressbar.streams.flush()
 from processor.TextProcessors import TextAnalyzer
 
-class DirectFeatures(object):
-    '''
-    Class for extracting features from text
-    '''
+class Features(object):
     
     _train=None
     _test=None
-    _direct_features=None
-    _direct_save_path=None
+    _save_path=None
+    _features_data_frame=None
+    
+    def __init__(self,params):
+        
+        if 'train' in params and 'test' in params:
+            self._train = pd.read_csv(params['train'])
+            self._test = pd.read_csv(params['test'])
+            
+class DirectFeatures(Features):
+    '''
+    Class for extracting features from text
+    '''
+  
     nasties=set()
     
     def __init__(self, params):
     
-        if 'train' in params and 'test' in params:
-            self._train = pd.read_csv(params['train'])
-            self._test = pd.read_csv(params['test'])
+        super(DirectFeatures, self).__init__(params)
         
-        self._direct_save_path='../../data/cleaned/direct_features.csv'
-        if os.path.exists(self._direct_save_path):
+        self._save_path='../../data/cleaned/direct_features.csv'
+        if os.path.exists(self._save_path):
             if 'purge' in params:
                 if not params['purge']:
-                    self._direct_features=pd.read_csv(self._direct_save_path)
+                    self._features_data_frame=pd.read_csv(self._save_path)
                 else:
-                    os.remove(self._direct_save_path)
+                    os.remove(self._save_path)
             else:
-                self._direct_features=pd.read_csv(self._direct_save_path)
+                self._features_data_frame=pd.read_csv(self._save_path)
                 
         if 'nasty_path' in params:
             with open(params['nasty_path'],'rb') as nasty:
@@ -44,38 +51,86 @@ class DirectFeatures(object):
         if 'nasties' in params:
             self.nasties=params['nasties']
             
-    def generate_direct_features(self):  
+    def generate_features_data_frame(self):  
         
-        if self._direct_features is None:  
-            self._direct_features = pd.concat([self._train.iloc[:,1:3],self._test.iloc[:,1:3]])
-            self._direct_features=self._direct_features.reset_index(drop=True)
+        if self._features_data_frame is None:  
+            self._features_data_frame = pd.concat([self._train.iloc[:,1:3],self._test.iloc[:,1:3]])
+            self._features_data_frame=self._features_data_frame.reset_index(drop=True)
     
         bar = progressbar.ProgressBar(widgets=[' [', progressbar.Timer(), '] ',progressbar.Bar(),' (', progressbar.ETA(), ') ',])
         ta=TextAnalyzer({})
-        for index in bar(range(len(self._direct_features))):
-            row=self._direct_features.loc[index]
+        for index in bar(range(len(self._features_data_frame))):
+            row=self._features_data_frame.loc[index]
             comment=row.comment_text
             num_sentences,num_words,num_unique_words,num_letters,num_punctuations,num_uppers,num_titles,num_stops,mean_length,num_nasties,num_emojis = ta.get_metrics(comment,self.nasties)
-            self._direct_features.loc[index, 'number_of_sentences'] = num_sentences
-            self._direct_features.loc[index, 'number_of_words'] = num_words
-            self._direct_features.loc[index, 'number_of_unique_words'] = num_unique_words
-            self._direct_features.loc[index, 'number_of_letters'] = num_letters
-            self._direct_features.loc[index, 'number_of_punctuations'] = num_punctuations
-            self._direct_features.loc[index, 'number_of_uppercase_words'] = num_uppers
-            self._direct_features.loc[index, 'number_of_title_words'] = num_titles
-            self._direct_features.loc[index, 'number_of_stop_words'] = num_stops
-            self._direct_features.loc[index, 'mean_length_of_words'] = mean_length
-            self._direct_features.loc[index, 'number_of_nasty_words'] = num_nasties
-            self._direct_features.loc[index, 'number_of_emojis'] = num_emojis
-            self._direct_features.loc[index, 'percentage_of_unique_words'] = (100*num_unique_words/num_words)
-            self._direct_features.loc[index, 'percentage_of_punctuations'] = (100*num_punctuations/num_words)
-            self._direct_features.loc[index, 'percentage_of_nasty_words'] = (100*num_nasties/num_words)
-            self._direct_features.loc[index, 'percentage_of_emojis'] = (100*num_emojis/num_words)
+            self._features_data_frame.loc[index, 'number_of_sentences'] = num_sentences
+            self._features_data_frame.loc[index, 'number_of_words'] = num_words
+            self._features_data_frame.loc[index, 'number_of_unique_words'] = num_unique_words
+            self._features_data_frame.loc[index, 'number_of_letters'] = num_letters
+            self._features_data_frame.loc[index, 'number_of_punctuations'] = num_punctuations
+            self._features_data_frame.loc[index, 'number_of_uppercase_words'] = num_uppers
+            self._features_data_frame.loc[index, 'number_of_title_words'] = num_titles
+            self._features_data_frame.loc[index, 'number_of_stop_words'] = num_stops
+            self._features_data_frame.loc[index, 'mean_length_of_words'] = mean_length
+            self._features_data_frame.loc[index, 'number_of_nasty_words'] = num_nasties
+            self._features_data_frame.loc[index, 'number_of_emojis'] = num_emojis
+            self._features_data_frame.loc[index, 'percentage_of_unique_words'] = (100*num_unique_words/num_words)
+            self._features_data_frame.loc[index, 'percentage_of_punctuations'] = (100*num_punctuations/num_words)
+            self._features_data_frame.loc[index, 'percentage_of_nasty_words'] = (100*num_nasties/num_words)
+            self._features_data_frame.loc[index, 'percentage_of_emojis'] = (100*num_emojis/num_words)
         
-        self._direct_features.to_csv(self._direct_save_path)
+        self._features_data_frame.to_csv(self._direct_save_path)
     
-    def get_direct_features(self):
-        return self._direct_features
+    def get_features_data_frame(self):
+        return self._features_data_frame
+    
+    def get_train(self):
+        return self._train
+    
+    def get_test(self):
+        return self._test
+    
+class DerivedFeatures(Features):
+    '''
+    Class for extracting derived features from text
+    '''
+   
+    corpus=None
+    
+    def __init__(self, params):
+    
+        super(DerivedFeatures, self).__init__(params)
+        
+        self._save_path='../../data/cleaned/derived_features.csv'
+        if os.path.exists(self._save_path):
+            if 'purge' in params:
+                if not params['purge']:
+                    self._features_data_frame=pd.read_csv(self._save_path)
+                else:
+                    os.remove(self._save_path)
+            else:
+                self._features_data_frame=pd.read_csv(self._save_path)
+            
+            self.corpus=self._features_data_frame.comment_text
+            
+    def generate_features_data_frame(self):  
+        
+        if self._features_data_frame is None:  
+            self._features_data_frame = pd.concat([self._train.iloc[:,1:3],self._test.iloc[:,1:3]])
+            self._features_data_frame=self._features_data_frame.reset_index(drop=True)
+            self.corpus=self._features_data_frame.comment_text
+    
+        bar = progressbar.ProgressBar(widgets=[' [', progressbar.Timer(), '] ',progressbar.Bar(),' (', progressbar.ETA(), ') ',])
+        ta=TextAnalyzer({})
+        for index in bar(range(len(self.corpus))):
+            row=self._features_data_frame.loc[index]
+            comment=row.comment_text
+            self.corpus.loc[index, 'sanitized_comment'] = ta.get_sanitized(comment)
+            
+        self.corpus.to_csv(self._direct_save_path)
+    
+    def get_features_data_frame(self):
+        return self._features_data_frame
     
     def get_train(self):
         return self._train
--- src/processor/TextProcessors.py
+++ src/processor/TextProcessors.py
@@ -8,20 +8,90 @@ import string
 import numpy as np
 import emoji
 import re
+from nltk import pos_tag
 from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer 
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import TweetTokenizer
 
 class TextAnalyzer(object):
     '''
     Class for analyzing certain properties of text
     '''
     my_stopwords=None
-
+    my_apostrophes=None
+    my_tokenizer=None
+    my_lemmatizer=None
+    
     def __init__(self, params):
         '''
         Constructor
         '''
         nltk.data.path.append('../../nltkData')
+        self.my_tokenizer=TweetTokenizer()
+        self.my_lemmatizer= WordNetLemmatizer()
         self.my_stopwords = set(stopwords.words("english"))
+        self.my_apostrophes={
+                            "aren't" : "are not",
+                            "can't" : "cannot",
+                            "couldn't" : "could not",
+                            "didn't" : "did not",
+                            "doesn't" : "does not",
+                            "don't" : "do not",
+                            "hadn't" : "had not",
+                            "hasn't" : "has not",
+                            "haven't" : "have not",
+                            "he'd" : "he would",
+                            "he'll" : "he will",
+                            "he's" : "he is",
+                            "i'd" : "I would",
+                            "i'd" : "I had",
+                            "i'll" : "I will",
+                            "i'm" : "I am",
+                            "isn't" : "is not",
+                            "it's" : "it is",
+                            "it'll":"it will",
+                            "i've" : "I have",
+                            "let's" : "let us",
+                            "mightn't" : "might not",
+                            "mustn't" : "must not",
+                            "shan't" : "shall not",
+                            "she'd" : "she would",
+                            "she'll" : "she will",
+                            "she's" : "she is",
+                            "shouldn't" : "should not",
+                            "that's" : "that is",
+                            "there's" : "there is",
+                            "they'd" : "they would",
+                            "they'll" : "they will",
+                            "they're" : "they are",
+                            "they've" : "they have",
+                            "we'd" : "we would",
+                            "we're" : "we are",
+                            "weren't" : "were not",
+                            "we've" : "we have",
+                            "what'll" : "what will",
+                            "what're" : "what are",
+                            "what's" : "what is",
+                            "what've" : "what have",
+                            "where's" : "where is",
+                            "who'd" : "who would",
+                            "who'll" : "who will",
+                            "who're" : "who are",
+                            "who's" : "who is",
+                            "who've" : "who have",
+                            "won't" : "will not",
+                            "wouldn't" : "would not",
+                            "you'd" : "you would",
+                            "you'll" : "you will",
+                            "you're" : "you are",
+                            "you've" : "you have",
+                            "'re": " are",
+                            "wasn't": "was not",
+                            "we'll":" will",
+                            "didn't": "did not",
+                            "tryin'":"trying"
+                            }
     
     def get_metrics(self,intext,nasties=set()):
         sentence_list = nltk.tokenize.sent_tokenize(intext)
@@ -67,3 +137,25 @@ class TextAnalyzer(object):
         if len(emoji_list)>0:
             print('Found emojis ',emoji_list)
         return len(emoji_list)
+    
+    def get_sanitized(self,input):
+        
+        comment=input.lower()
+        #remove \n
+        comment=re.sub("\\n","",comment)
+        # remove leaky elements like ip,user
+        comment=re.sub("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}","",comment)
+        #removing usernames
+        comment=re.sub("\[\[.*\]","",comment)
+        
+        words=self.my_tokenizer.tokenize(comment)
+        
+        words=[self.my_apostrophes[word] if word in self.my_apostrophes else word for word in words]
+        words=[self.my_lemmatizer.lemmatize(word, "v") for word in words]
+        words = [w for w in words if not w in self.my_stopwords]
+        
+        clean_sent=" ".join(words)
+        # remove any non alphanum,digit character
+        clean_sent=re.sub("\W+"," ",clean_sent)
+        clean_sent=re.sub("  "," ",clean_sent)
+        return(clean_sent)
